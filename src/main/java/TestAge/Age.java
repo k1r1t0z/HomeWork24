b39a45cd2dd07de3452a70235ba0c6a931701946
kiritoz <kiritoz@mail.ru>
2023-07-24 19:00:00 +0300

    Delete "Scanner"

--- src/main/java/TestAge/Age.java
+++ src/main/java/TestAge/Age.java
@@ -8,13 +8,11 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Scanner;
 
 @WebServlet("/age")
 public class Age extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Scanner scanner = new Scanner(System.in);
         resp.setContentType("text/html");
         PrintWriter writer = resp.getWriter();
         int age = Integer.parseInt(req.getParameter("age"));
